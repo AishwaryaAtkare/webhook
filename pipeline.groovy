ced6543d3c143560549fafa9eba79493b2ca28d0
aishwarya <aishwarya@gmail.com>
2022-04-05 17:13:19 +0530

    hjdbf

--- pipeline.groovy
+++ pipeline.groovy
@@ -16,5 +16,10 @@ pipeline {
                 echo "Aish"
             }
         }
+        stage('proc') { 
+            steps {
+                echo "final product"
+            }
+        }
     }
 }
